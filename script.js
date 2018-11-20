77b7287758685a356d3a5eaeebb7fffa7b1fa8fc
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-19 20:46:50 -0800

    added bubble...popping bubble works but still need to understand so commented out

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ let createBubble = function() {
   let newP = document.createElement('p');
   newP.textContent = bubbleText;
   newP.className = 'bubble-text';
-
   let newDiv = document.createElement('div');
   newDiv.className = 'bubble bubble__small';
   newDiv.append(newP);
-
   document.querySelector('main').append(newDiv);
-
 };
 
 button.addEventListener('click', function() {
   createBubble();
+});
+
+let popBubble = function(e) {
+  console.log(e);
+  // let classes = Array.from(e.target.classList);
+  // if (classes.includes('bubble')) {
+  //   e.target.remove();
+  // } else if (classes.includes('bubble-text')) {
+  //   e.target.parentNode.remove();
+  // };
+};
+
+window.addEventListener('click', function(e) {
+  popBubble(e);
 });
